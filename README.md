40dae484fd3bcda83223df1af2f2e1f4c0cd0e97
“Kgotso-Koete” <kgkoete@gmail.com>
2022-09-17 19:48:54 +0100

    minor edit

---
+++ README.md
@@ -0,0 +1,46 @@
+# About the project
+
+### Description of the project
+
+This is a clone of the video game [Flappy Bird](https://en.wikipedia.org/wiki/Flappy_Bird) built while following the [Clear Code tutorial part 1](https://youtu.be/UZg49z76cLw) and [Clear Code tutorial part 2](https://youtu.be/XRw1FUEsSv4). [Flappy Bird](https://en.wikipedia.org/wiki/Flappy_Bird)is a mobile game developed by Vietnamese video game artist and programmer Dong Nguyen (Vietnamese: Nguyễn Hà Đông), under his game development company .Gears. The game is a side-scroller where the player controls a bird, attempting to fly between columns of green pipes without hitting them
+
+---
+
+### Technology stack
+
+1. Python 3.10.4
+2. Pygame 2.1.2
+
+---
+
+### Install on Linux or Windows:
+
+1. Download this codebase
+2. Install Python 3.10.4 on your machine
+3. Install Pygame 2.1.2
+
+### 3: Run project
+
+Navigate to the project folder and run `python3 main.py` to start the game
+
+---
+
+### Acknowledgements
+
+Special thanks to [Clear Code](https://www.youtube.com/c/ClearCode) for a great tutorial. The tutorial was explained very well, brief and very impactful in showing the basics of Pygame.
+
+The tutorial also has a link to the the [original image files](https://github.com/samuelcust/flappy-bird-assets) and the [original sound files](https://www.sounds-resource.com/mobile/flappybird/sound/5309/). The github repo for the actual tutorial can be found [here](https://github.com/clear-code-projects/FlappyBird_Python)
+<br/>
+<br/>
+
+---
+
+### License
+
+The codebase is MIT licensed unless otherwise specified. Feel free to fork or download this code and use as specified in the license.
+
+#
+
+To be modified further by Kgotso Koete
+<br/>
+September 2022
--- main.py
+++ main.py
@@ -19,7 +19,8 @@ def create_pipe():
 def move_pipes(pipes):
     for pipe in pipes:
         pipe.centerx -= 5  # moves all pipes to the left by 5 pixels
-    return pipes
+    visible_pipes = [pipe for pipe in pipes if pipe.right > -50]
+    return visible_pipes
 
 
 def draw_pipes(pipes):
@@ -32,12 +33,16 @@ def draw_pipes(pipes):
 
 
 def check_collision(pipes):
+    global can_score
+
     for pipe in pipes:
         if bird_rect.colliderect(pipe):
             death_sound.play()
+            can_score = True
             return False
 
     if bird_rect.top <= -100 or bird_rect.bottom >= 900:
+        can_score = True
         return False
 
     return True
@@ -78,7 +83,19 @@ def update_score(score, high_score):
     return score
 
 
-pygame.mixer.pre_init(frequency=44100, size=16, channels=2, buffer=512)
+def pipe_score_check():
+    global score, can_score
+    if pipe_list:
+        for pipe in pipe_list:
+            if 95 < pipe.centerx < 109 and can_score:
+                score += 1
+                score_sound.play()
+                can_score = False
+            if pipe.centerx < 0:
+                can_score = True
+
+
+# pygame.mixer.pre_init(frequency=44100, size=16, channels=2, buffer=512)
 pygame.init()
 screen = pygame.display.set_mode((576, 1024))
 clock = pygame.time.Clock()
@@ -124,6 +141,7 @@ pipe_height = [400, 600, 800]
 game_active = True
 score = 0
 high_score = 0
+can_score = True
 
 game_over_surface = pygame.image.load("assets/message.png").convert_alpha()
 game_over_surface = pygame.transform.scale2x(game_over_surface)
@@ -181,12 +199,9 @@ if __name__ == "__main__":
             pipe_list = move_pipes(pipe_list)
             draw_pipes(pipe_list)
 
-            score += 0.01
+            # Score
+            pipe_score_check()
             score_display("main_game")
-            score_sound_countdown -= 1
-            if score_sound_countdown <= 0:
-                score_sound.play()
-                score_sound_countdown = 100
         else:
             screen.blit(game_over_surface, game_over_rect)
             high_score = update_score(score, high_score)
